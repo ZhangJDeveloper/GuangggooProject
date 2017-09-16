9ab89b2c04b1422e36d82ce6dd9ad464120efadf
张杰 <zhangjie@zhangjie-imac.local>
2017-09-16 17:29:20 +0800

    Initial Commit

---
+++ GuangggooProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GuangggooProject
+//
+//  Created by ZhangJie on 2017/9/16.
+//  Copyright © 2017年 nuoyuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
